589130d17b91904d7171ba5c33f61a7c71d73fe6
j-gottlieb <gessejottlieb@gmail.com>
2018-12-03 09:18:13 -0500

    npm install tunajs
    Created chorus and filter objects and connected them to the signal chain

--- assets/scripts/synth.js
+++ assets/scripts/synth.js
@@ -1,3 +1,4 @@
+import Tuna from 'tunajs'
 const frequencies = require('./frequencies')
 
 const synthKeys = {}
@@ -9,14 +10,32 @@ class Synth {
     this.audioContext = new AudioContext()
     this.oscillator = this.audioContext.createOscillator()
     this.gain = this.audioContext.createGain()
+    this.oscillator.frequency.value = this.pitch
+    this.oscillator.type = 'square'
     this.gain.gain.value = 0.0
+    this.tuna = new Tuna(this.audioContext)
     this.oscillator.start()
+    this.filter = new this.tuna.Filter({
+      frequency: 440, // 20 to 22050
+      Q: 1, // 0.001 to 100
+      gain: 0, // -40 to 40 (in decibels)
+      filterType: 'lowpass', // lowpass, highpass, bandpass, lowshelf, highshelf, peaking, notch, allpass
+      bypass: 0
+    })
+
+    this.chorus = new this.tuna.Chorus({
+      rate: 4, // 0.01 to 8+
+      feedback: 0.4, // 0 to 1+
+      delay: 0.5, // 0 to 1
+      bypass: 0 // the value 1 starts the effect as bypassed, 0 or 1
+    })
+    this.oscillator.connect(this.chorus)
+    this.chorus.connect(this.filter)
+    this.filter.connect(this.gain)
   }
 
   synthOn () {
     this.gain.gain.setTargetAtTime(0.8, this.audioContext.currentTime, 0.02)
-    this.oscillator.frequency.value = this.pitch
-    this.oscillator.connect(this.gain)
     this.gain.connect(this.audioContext.destination)
   }
 
