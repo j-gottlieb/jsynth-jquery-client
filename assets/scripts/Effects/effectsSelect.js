76821c6065d8f8a3e5d87d77728b548e4be1b014
j-gottlieb <gessejottlieb@gmail.com>
2018-12-04 16:43:47 -0500

    Created default select value
    if user has no settings saved, the select will show "create some settings!"

--- assets/scripts/Effects/effectsSelect.js
+++ assets/scripts/Effects/effectsSelect.js
@@ -1,10 +1,14 @@
 const store = require('../store.js')
 
 const populateSelect = function () {
-  $('#effects-select').html('')
-  store.settings.map(setting => {
-    $('#effects-select').append(`<option value=${setting.name}>${setting.name}</option>`)
-  })
+  if (store.settings.length === 0) {
+    $('#effects-select').html('<option value="" disabled selected>Create some settings!</option>')
+  } else {
+    $('#effects-select').html('')
+    store.settings.map(setting => {
+      $('#effects-select').append(`<option value=${setting.name}>${setting.name}</option>`)
+    })
+  }
 }
 
 module.exports = {
